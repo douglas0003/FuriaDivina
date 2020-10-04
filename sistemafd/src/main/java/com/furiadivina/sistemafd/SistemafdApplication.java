43623af51eac02b1aca96dffa19e7eb0ca8bb5d1
Douglas Ferreira <douglas0003@hotmail.com>
2020-10-04 19:29:19 -0300

    commit inicial do projeto Furia Divina

---
+++ sistemafd/src/main/java/com/furiadivina/sistemafd/SistemafdApplication.java
@@ -0,0 +1,13 @@
+package com.furiadivina.sistemafd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemafdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemafdApplication.class, args);
+	}
+
+}
---
+++ sistemafd/src/test/java/com/furiadivina/sistemafd/SistemafdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furiadivina.sistemafd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemafdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
